770d4bf5de8965b809ef9cd02c39059c47e3d742
pranishkarki20 <praniskkarki20@gmail.com>
2026-06-08 17:32:45 +0530

    update

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -74,19 +74,19 @@ export default function Contact() {
             />
 
             <input
-              type="tel"
+              type="text"
               name="phone"
               placeholder='Phone Number'
               value={formData.phone}
               onChange={handlechange}
             />
 
-            <input
-            type="text"
-            name = "description"
-            value= {formData.description}
-            placeholder='Any Queries'
-            onChange={handlechange}/>
+            <textarea
+              name="description"
+              value={formData.description}
+              placeholder='Any Queries'
+              onChange={handlechange}
+            />
             <motion.button
               whileHover={{ scale: 1.1 }}
               type="submit"
@@ -97,7 +97,6 @@ export default function Contact() {
           </motion.div>
 
           <div className='ncontainer'>
-            <h1 className="text-center">Contact Information</h1>
             <h2 className="info-label">Phone Number</h2>
             <p className="info-value">+91 9036327418</p>
             <h2 className="info-label">Email</h2>
